238c0c0cfd22c4fbd5d0154d4a1eab624db6657e
Murilo Marques <100244178+MuriloMarques621@users.noreply.github.com>
2022-06-12 16:35:56 -0300

    Add files via upload

---
+++ layout/produtos.js
@@ -0,0 +1,266 @@
+var db_contatos_inicial = {
+    "data": [
+        {
+            "id": 1,
+            "nome": "Mouse Gamer Razer Deathadder V2 Chroma",
+            "loja": "Kabum",
+            "categoria": "Perifericos",
+            "marca": "Razer",
+            "preco": "259,90",
+            "imagem": "imagens/mouse.jpg",
+            "website": "https://www.kabum.com.br/produto/109815/mouse-gamer-razer-deathadder-v2-chroma-optical-switch-8-botoes-20000dpi-rz01-03210100-r3u1"
+        },
+        {
+            "id": 2,
+            "nome": "Mouse Gamer Razer Deathadder V2 Chroma",
+            "loja": "Amazon",
+            "categoria": "Perifericos",
+            "marca": "Razer",
+            "preco": "328,26",
+            "imagem": "imagens/mouse.jpg",
+            "website": "https://www.amazon.com.br/Mouse-Gaming-Razer-DeathAdder-Program%C3%A1veis/dp/B082G5SPR5/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=14W29TMPJQI5&keywords=Deathadder+V2+Chroma&qid=1653153431&sprefix=deathadder+v2+chroma%2Caps%2C239&sr=8-1&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147"
+        },
+        {
+            "id": 3,
+            "nome": "Mouse Gamer Razer Deathadder V2 Chroma",
+            "loja": "Mercado Livre",
+            "categoria": "Perifericos",
+            "marca": "Razer",
+            "preco": "303,00",
+            "imagem": "imagens/mouse.jpg",
+            "website": "https://www.mercadolivre.com.br/mouse-para-jogo-razer-deathadder-v2-preto/p/MLB15698869#searchVariation=MLB15698869&position=1&search_layout=stack&type=product&tracking_id=1841997d-f4fa-4349-baed-7a8037eae401"
+        },
+        {
+            "id": 4,
+            "nome": "Mouse Gamer Razer Deathadder V2 Chroma",
+            "loja": "Pichau",
+            "categoria": "Perifericos",
+            "marca": "Razer",
+            "preco": "299,90",
+            "imagem": "imagens/mouse.jpg",
+            "website": "https://www.pichau.com.br/mouse-razer-deathadder-v2-chroma-20000-dpi-rz01-03210100-r3u1"
+        },
+        {
+            "id": 5,
+            "nome": "Mouse Gamer Razer Deathadder V2 Chroma",
+            "loja": "Magazine Luiza",
+            "categoria": "Perifericos",
+            "marca": "Razer",
+            "preco": "359,90",
+            "imagem": "imagens/mouse.jpg",
+            "website": "https://www.magazineluiza.com.br/mouse-gamer-razer-deathadder-v2-chroma-8-botoes-20-000dpi-preto/p/bg28b3ck45/in/mger/"
+        },
+        {
+            "id": 6,
+            "nome": "Mouse Gamer Razer Deathadder V2 Chroma",
+            "loja": "Americanas",
+            "categoria": "Perifericos",
+            "marca": "Razer",
+            "preco": "388,90",
+            "imagem": "imagens/mouse.jpg",
+            "website": "https://www.americanas.com.br/produto/4814471119?pfm_carac=mouse-razer-deathadder-v2-chroma&pfm_page=search&pfm_pos=grid&pfm_type=search_page&offerId=626aa0ed87c00289c24765ea&cor=Preto"
+        },
+        {
+            "id": 7,
+            "nome": "iPhone 12 Apple",
+            "loja": "Americanas",
+            "categoria": "Mobile",
+            "marca": "Apple",
+            "preco": "5017,57",
+            "imagem": "imagens/Iphone.webp",
+            "website": "https://www.americanas.com.br/produto/3591024100?pfm_carac=iphone-12-apple-64gb-branco-tela-6-1-12mp-ios&pfm_page=search&pfm_pos=grid&pfm_type=search_page&offerId=6139379009c351890d7a3df1&cor=BRANCO"
+        },
+        {
+            "id": 8,
+            "nome": "iPhone 12 Apple",
+            "loja": "Mercado Livre",
+            "categoria": "Mobile",
+            "marca": "Apple",
+            "preco": "5999,90",
+            "imagem": "imagens/Iphone.webp",
+            "website": "https://produto.mercadolivre.com.br/MLB-2071914014-iphone-12-mini-apple-64gb-branco-tela-54-cm-dupla-12mp-_JM#position=3&search_layout=stack&type=item&tracking_id=d509c9bd-95ec-4c5d-bdb4-19d19292596c"
+        },
+        {
+            "id": 9,
+            "nome": "iPhone 12 Apple",
+            "loja": "Kabum",
+            "categoria": "Mobile",
+            "marca": "Apple",
+            "preco": "4789,00",
+            "imagem": "imagens/Iphone.webp",
+            "website": "https://www.kabum.com.br/produto/346773/iphone-12-mini-apple-64gb-verde-5-4-cam-dupla-12mp-ios"
+        },
+        {
+            "id": 10,
+            "nome": "iPhone 12 Apple",
+            "loja": "Amazon",
+            "categoria": "Mobile",
+            "marca": "Apple",
+            "preco": "5049,99",
+            "imagem": "imagens/Iphone.webp",
+            "website": "https://www.amazon.com.br/Apple-iPhone-12-64-GB-Branco/dp/B09B7X1814/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=WQBFQ53YF02N&keywords=iPhone%2B12%2BApple%2B128GB%2B-%2BBranco%2BTela%2B6%2C1%E2%80%9D%2B12MP%2BiOS&qid=1653160398&sprefix=iphone%2B12%2Bapple%2B64gb%2B-%2Bpreto%2Btela%2B6%2B1%2B12mp%2Bios%2Caps%2C206&sr=8-1&ufe=app_do%3Aamzn1.fos.95de73c3-5dda-43a7-bd1f-63af03b14751&th=1"
+        },
+        {
+            "id": 11,
+            "nome": "iPhone 12 Apple",
+            "loja": "Magazine Luiza",
+            "categoria": "Mobile",
+            "marca": "Apple",
+            "preco": "5207,07",
+            "imagem": "imagens/Iphone.webp",
+            "website": "https://www.magazineluiza.com.br/iphone-12-apple-64gb-branco-tela-61-12mp-ios/p/231147400/te/teip/"
+        },
+        {
+            "id": 12,
+            "nome": "Gigabyte GeForce rtx 2060",
+            "loja": "Americanas",
+            "categoria": "Peças",
+            "marca": "Nvidia",
+            "preco": "3085,90",
+            "imagem": "imagens/Placa.webp",
+            "website": "https://www.americanas.com.br/produto/4069197260?pfm_carac=placa-de-video-gigabyte-geforce-rtx-2060-d6-6gb-gddr6&pfm_page=search&pfm_pos=grid&pfm_type=search_page&offerId=6271726487c00289c2db903a"
+        },
+        {
+            "id": 13,
+            "nome": "Gigabyte GeForce rtx 2060",
+            "loja": "Magazine Luiza",
+            "categoria": "Peças",
+            "marca": "Nvidia",
+            "preco": "3190,90",
+            "imagem": "imagens/Placa.webp",
+            "website": "https://www.magazineluiza.com.br/placa-de-video-gigabyte-geforce-rtx-2060-d6-6gb-gddr6-192-bit-gv-n2060d6-6gd-rev-2-0/p/hcfek08e48/in/pcvd/"
+        },
+        {
+            "id": 14,
+            "nome": "Gigabyte GeForce rtx 2060",
+            "loja": "Pichau",
+            "categoria": "Peças",
+            "marca": "Nvidia",
+            "preco": "2899,90",
+            "imagem": "imagens/Placa.webp",
+            "website": "https://www.pichau.com.br/placa-de-video-gigabyte-geforce-rtx-2060-d6-6gb-gddr6-192-bit-gv-n2060d6-6gd-rev-2-0"
+        },
+        {
+            "id": 15,
+            "nome": "Gigabyte GeForce rtx 2060",
+            "loja": "Amazon",
+            "categoria": "Peças",
+            "marca": "Nvidia",
+            "preco": "2931,60",
+            "imagem": "imagens/Placa.webp",
+            "website": "https://www.amazon.com.br/Placa-V%C3%ADdeo-Gigabyte-GeForce-GDDR6/dp/B095SWPGVR/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2LSQJYLITNNZH&keywords=PLACA+DE+VIDEO+GIGABYTE+GEFORCE+RTX+2060+D6%2C+6GB%2C+GDDR6&qid=1653159744&sprefix=placa+de+video+gigabyte+geforce+rtx+2060+d6+6gb+gddr6%2Caps%2C380&sr=8-1&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147"
+        },
+        {
+            "id": 16,
+            "nome": "Gigabyte GeForce rtx 2060",
+            "loja": "Kabum",
+            "categoria": "Peças",
+            "marca": "Nvidia",
+            "preco": "3399,00",
+            "imagem": "imagens/Placa.webp",
+            "website": "https://www.kabum.com.br/produto/302344/placa-de-video-gigabyte-geforce-rtx-2060-d6-6gb-gddr6-192bit"
+        },
+        {
+            "id": 17,
+            "nome": "Gigabyte GeForce rtx 2060",
+            "loja": "Mercado Livre",
+            "categoria": "Peças",
+            "marca": "Nvidia",
+            "preco": "3299,00",
+            "imagem": "imagens/Placa.webp",
+            "website": "https://www.mercadolivre.com.br/placa-de-video-nvidia-gigabyte-geforce-rtx-20-series-rtx-2060-gv-n2060d6-6gd-rev-20-6gb/p/MLB18617723?pdp_filters=category:MLB1712#searchVariation=MLB18617723&position=2&search_layout=grid&type=product&tracking_id=697bcc12-f54b-4335-b81e-877e75374ad5"
+        },
+        {
+            "id": 18,
+            "nome": "Notebook Gamer GIGABYTE G5",
+            "loja": "Kabum",
+            "categoria": "Computadores",
+            "marca": "GIGABYTE",
+            "preco": "8199,90",
+            "imagem": "imagens/Notebook.webp",
+            "website": "https://www.kabum.com.br/produto/269069/notebook-gamer-gigabyte-g5-geforce-rtx-3050-ti-intel-core-i5-11400h-16gb-ram-ssd-512gb-15-6-fhd-144hz-md-51br123sd"
+        },
+        {
+            "id": 19,
+            "nome": "Notebook Gamer GIGABYTE G5",
+            "loja": "Pichau",
+            "categoria": "Computadores",
+            "marca": "GIGABYTE",
+            "preco": "8499,90",
+            "imagem": "imagens/Notebook.webp",
+            "website": "https://www.pichau.com.br/notebook-gamer-gigabyte-g5-kd-intel-i5-11400h-15-6-fhd-144hz-16gb-ddr4-ssd-512gb-m-2-rtx-3060-6gb-kd-52br123sd"
+        },
+        {
+            "id": 20,
+            "nome": "Notebook Gamer GIGABYTE G5",
+            "loja": "Americanas",
+            "categoria": "Computadores",
+            "marca": "GIGABYTE",
+            "preco": "9499,90",
+            "imagem": "imagens/Notebook.webp",
+            "website": "https://www.americanas.com.br/produto/4822007885?pfm_carac=notebook-gamer-gigabyte-g5&pfm_page=search&pfm_pos=grid&pfm_type=search_page&offerId=6234dc2187c00289c2c46dd5&cor=Preto"
+        },
+        {
+            "id": 21,
+            "nome": "Notebook Gamer GIGABYTE G5",
+            "loja": "Magazine Luiza",
+            "categoria": "Computadores",
+            "marca": "GIGABYTE",
+            "preco": "8631,47",
+            "imagem": "imagens/Notebook.webp",
+            "website": "https://www.magazineluiza.com.br/notebook-gamer-gigabyte-g5-geforce-rtx-3050-ti-intel-core-i5-11400h-16gb-ram-ssd-512gb-15-6-fhd-144hz-md-51br123sd/p/gckj1kgckg/in/note/"
+        },
+        {
+            "id": 22,
+            "nome": "Notebook Gamer GIGABYTE G5",
+            "loja": "Mercado Livre",
+            "categoria": "Computadores",
+            "marca": "GIGABYTE",
+            "preco": "11359,89",
+            "imagem": "imagens/Notebook.webp",
+            "website": "https://produto.mercadolivre.com.br/MLB-1942012485-notebook-gamer-gigabyte-g5-intei-i5-rtx3060-16g-ssd512-w10-_JM?searchVariation=90379793279#searchVariation=90379793279&position=3&search_layout=grid&type=item&tracking_id=0ea4255e-008a-45c8-8ba0-803276015483"
+        },
+        {
+            "id": 23,
+            "nome": "Notebook Gamer GIGABYTE G5",
+            "loja": "Amazon",
+            "categoria": "Computadores",
+            "marca": "GIGABYTE",
+            "preco": "9658,90",
+            "imagem": "imagens/Notebook.webp",
+            "website": "https://www.amazon.com.br/Notebook-Gamer-Gigabyte-I5-11400h-Kd-52br123sd/dp/B09W41TSBK/ref=sr_1_fkmr0_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2Z1CBEYF8VI6Y&keywords=Notebook+Gamer+GIGABYTE+G5+-+GeForce+RTX+3050+Ti%2C+Intel+Core+i5-11400H%2C+16GB+RAM%2C+SSD+512GB%2C+15.6+FHD+144Hz+-+MD-51BR123SD&qid=1653159971&sprefix=notebook+gamer+gigabyte+g5+-+geforce+rtx+3050+ti+intel+core+i5-11400h+16gb+ram+ssd+512gb+15.6+fhd+144hz+-+md-51br123sd%2Caps%2C169&sr=8-1-fkmr0&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147"
+        }
+    ]
+}
+
+
+function listar(categoria) {
+    let lista_produtos = '';
+    for (let index = 0; index < db_contatos_inicial.data.length; index++) {
+        const produto = db_contatos_inicial.data[index];
+        const elemento = `
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <img src="${db_contatos_inicial.data[index].imagem}" class="card-img-top" alt="produto 1">
+                <div class="button">
+                    <h5 class="card-title"><b>${db_contatos_inicial.data[index].nome}</b></h5>
+                    <p class="card-text"></p>
+                    <p>Preço: ${db_contatos_inicial.data[index].preco}</p>
+                    <p>Marca: ${db_contatos_inicial.data[index].marca}</p>
+                    <p>Loja: ${db_contatos_inicial.data[index].loja}</p>
+                    <a href="${db_contatos_inicial.data[index].website}" target="_blank" class="link-produto btn btn-primary">Comprar</a>
+                </div>
+            </div>
+        </div>
+        `
+        if (categoria === 'Todos') {
+
+            lista_produtos += elemento
+        } else
+            if (produto.categoria === categoria) {
+                lista_produtos += elemento
+            }
+
+    }
+    document.getElementById("cards_produtos").innerHTML = lista_produtos
+}
